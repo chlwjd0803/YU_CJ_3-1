46d32b8d20ab78613e130de47b19ab192425d902
Jung Choi <chlwjd0803@naver.com>
2025-05-26 21:51:51 +0900

    [운영체제] 페이징 알고리즘 - FIFO완성, Optimal 문제 해결중

---
+++ HW5/PagingAlgorithm/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("frame의 용량을 입력하세요 : ");
+        int frameCapacity = sc.nextInt(); // 프레임의 용량
+        System.out.print("Reference String 길이를 입력해주세요 : ");
+        int refStrLen = sc.nextInt(); // 페이지번호 배열의 길이
+        System.out.print("Reference String에 들어갈 페이지 범위를 입력해주세요. (1~n) : ");
+        int scope = sc.nextInt(); // 나올 페이지 번호의 범위
+
+        int [] refStr = new int[refStrLen]; // 페이지번호 배열 선언만
+        for(int i=0; i<refStrLen; i++)
+            refStr[i] = (int)(Math.random() * scope) + 1;
+        System.out.println("불러올 페이지들 : " + Arrays.toString(refStr));
+
+        FIFO.algorithm(frameCapacity, refStr);
+        Optimal.algorithm(frameCapacity, refStr);
+
+
+        sc.close();
+    }
+}
