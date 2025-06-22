e1b776f7423f80bd219810d96f87aa0278fe30fb
jung choi <chlwjd0803@naver.com>
2025-06-22 20:55:04 +0900

    [알고리즘] LCS 수정했는데도 틀림

--- HW4/2/src/Main.java
+++ HW4/2/src/Main.java
@@ -20,15 +20,15 @@ public class Main {
 
 
         for(int i=1; i<=l1; i++) for(int j=1; j<=l2; j++){
-            if(s1.charAt(i-1) == s2.charAt(j-1)){
+            if(s1.charAt(i-1) == s2.charAt(j-1))
                 lcs[i][j] = lcs[i-1][j-1] + 1;
-                if(totalLen < lcs[i][j]){
-                    buf[totalLen] = s1.charAt(i-1);
-                    totalLen = lcs[i][j];
-                }
-            }
             else
                 lcs[i][j] = Math.max(lcs[i-1][j], lcs[i][j-1]);
+
+            if(totalLen < lcs[i][j]){
+                buf[totalLen] = s1.charAt(i-1);
+                totalLen = lcs[i][j];
+            }
         }
 
         char[] answer = new char[totalLen];
@@ -42,6 +42,6 @@ public class Main {
         String s2 = scanner.nextLine();
 
         String answer = solution(s1, s2);
-        System.out.print(answer + "\n" + answer.length());
+        System.out.print(answer.length() + "\n" + answer);
     }
 }
